53a590675f07c071c9b099a94267b8fd761cfcae
Diego Zablah <70168871+dzab5@users.noreply.github.com>
2022-03-16 23:06:20 +0100

    changed config

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_mysqldb import MySQL
+from flaskext.mysql import MySQL
 import yaml
+import os
 
 app = Flask(__name__)
 
-db = yaml.safe_load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+app.secret_key = os.urandom(16)
 
-mysql = MySQL(app)
+
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_DATABASE_HOST'] = db['mysql_host']
+app.config['MYSQL_DATABASE_USER'] = db['mysql_user']
+app.config['MYSQL_DATABASE_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DATABASE_DB'] = db['mysql_db']
+
+mysql = MySQL()
+mysql.init_app(app)
 
 def get_cursor():
-    cursor = mysql.connection.cursor()
+    cursor = mysql.get_db().cursor()
     return cursor
 
 @app.route('/')
@@ -27,7 +32,7 @@ def impressum():
 @app.route('/agent_tools')
 def agent_tools():
     cur = get_cursor()
-    cur.execute('''SELECT citizen_id, visitor_name FROM Visitor WHERE infected''')
+    cur.execute('SELECT citizen_id, visitor_name FROM Visitor WHERE infected')
     infected_people = cur.fetchall()
     # Struggling to figure out the query for places with infected visitors
     cur.execute('SELECT place_id, place_name FROM Places')
