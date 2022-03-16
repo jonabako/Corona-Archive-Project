4ecb58618dfda87e4d718482189c8357aa778853
Diego Zablah <70168871+dzab5@users.noreply.github.com>
2022-03-16 14:05:34 +0100

    advanced on template creation

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for , request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def landing():
     return render_template('index.html')
 
+@app.route('/impressum')
+def impressum():
+    return render_template('imprint.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
