6e8c95fe9a59b0b9b0b00129fd12a97f790052d8
Diego Zablah <70168871+dzab5@users.noreply.github.com>
2022-03-18 21:54:41 +0100

    included tests

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
 
+#mysql configuration
 db = yaml.safe_load(open('db.yaml'))
 app.config['MYSQL_DATABASE_HOST'] = db['mysql_host']
 app.config['MYSQL_DATABASE_USER'] = db['mysql_user']
 app.config['MYSQL_DATABASE_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DATABASE_DB'] = db['mysql_db']
 
+# init app
 mysql = MySQL()
 mysql.init_app(app)
 
+# function to que mysql cursor
 def get_cursor():
     cursor = mysql.get_db().cursor()
     return cursor
